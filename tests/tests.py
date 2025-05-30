5087ef2a1ffb4353bbb05fbb8ae372e08cfa02f8
Giulia Fabiani <giulia.fabiani01@gmail.com>
2025-05-30 17:00:56 +0200

    Make it possible to run tests without a project

--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Address, Category, Contact, Entity
+from djeography.models import Address, Category, Contact, Entity
 
 # Create your tests here.
 
